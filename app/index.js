a8baf193478b3194c0bc90330cdd013e16aeecdb
Evan Engels <evan.engels@gmail.com>
2017-02-20 21:06:53 -0600

    styled and started js variables

--- app/index.js
+++ app/index.js
@@ -1,1 +1,5 @@
 import 'whatwg-fetch';
+
+const searchInput = document.querySelector('.search-bar');
+const searchBtn = document.querySelector('.search-button');
+const searchResults = document.querySelector('.results');
