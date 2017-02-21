60471259b1bb9bbe194140ca84a4be18a21049a2
Evan Engels <evan.engels@gmail.com>
2017-02-21 13:35:56 -0600

    finished JS, completed search bar functionality

--- app/index.js
+++ app/index.js
@@ -1,6 +1,19 @@
 import 'whatwg-fetch';
-import 'fill-in-results';
+import movieResults from './fill-in-results';
 
 const searchInput = document.querySelector('.search-bar');
 const searchBtn = document.querySelector('.search-button');
 const searchResults = document.querySelector('.results');
+
+function searchForMovies(name) {
+fetch(`http://netflixroulette.net/api/api.php?title=${name}`)
+  .then((res) => {
+    return res.json();
+  }).then((data) => {
+    movieResults(searchResults, data);
+  });
+}
+
+  searchBtn.addEventListener('click', () => {
+    searchForMovies(searchInput.value);
+  })
